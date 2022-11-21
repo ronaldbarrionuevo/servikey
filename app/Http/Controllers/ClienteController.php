1244a9d90946ccad7d9eb1ddb03c5112820943fc
ronaldbarrionuevo <barrionuevoronald488@gmail.com>
2022-11-20 21:23:55 -0400

    route_cliente_bd

--- app/Http/Controllers/ClienteController.php
+++ app/Http/Controllers/ClienteController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Inertia\Inertia;
 
 class ClienteController extends Controller
 {
@@ -13,7 +14,7 @@ class ClienteController extends Controller
      */
     public function index()
     {
-        //
+        return Inertia::render('Cliente/Index');
     }
 
     /**
--- resources/js/Pages/Cliente/Index.vue
+++ resources/js/Pages/Cliente/Index.vue
@@ -1,7 +1,14 @@
 <script setup>
-
+import AppLayout from '@/Layouts/AppLayout.vue';
 </script>
 
 <template>
+    <AppLayout>
+        <template #header>
+            Cliente
+        </template>
+        <h1>hola mundo </h1>
+    </AppLayout>
+    
     
 </template>
