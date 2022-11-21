3da0883483bed49e505a86685013ba0a4db03dcb
ronaldbarrionuevo <barrionuevoronald488@gmail.com>
2022-11-20 20:55:52 -0400

    migra_model_cliente

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -20,5 +20,7 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        \App\Models\Cliente::factory(10)->create();
     }
 }
