2ac07daf2f89ba89c02bfa45f64fd73578cad69e
ronaldbarrionuevo <barrionuevoronald488@gmail.com>
2022-11-23 00:23:12 -0400

    create_cleinte_falta

--- resources/js/Pages/Cliente/Create.vue
+++ resources/js/Pages/Cliente/Create.vue
@@ -1,11 +1,18 @@
 <script setup>
 import AppLayout from '@/Layouts/AppLayout.vue';
-import FormSection from '../../Components/FormSection.vue';
-import InputLabel from '../../Components/InputLabel.vue';
-import TextInput from '../../Components/TextInput.vue'
-function enviar(){
+import FormSection from '@/Components/FormSection.vue';
+import InputLabel from '@/Components/InputLabel.vue';
+import TextInput from '@/Components/TextInput.vue';
+import InputError from '@/Components/InputError.vue';
+import { useForm } from '@inertiajs/inertia-vue3';
 
-}
+const form = useForm({
+    descripcion_cliente:'',
+    telefono : '',
+});
+function enviar(){
+    console.log('FORMUALARIO ');
+};
 </script>
 <template>
     <AppLayout>
@@ -18,15 +25,19 @@ function enviar(){
                     <template #form>
                         <div class="col-span-6 sm:col-span-4" >
                             <InputLabel for="descripcion_cliente" value="Descripcion Cliente" />
-                            <TextInput type="text" name="descripcion_cliente" id="descripcion_cliente"></TextInput>
+                            <TextInput type="text" name="descripcion_cliente" id="descripcion_cliente" v-model="form.descripcion_cliente" class="w-full"></TextInput>
+                            <InputError v-bind:message="form.errors.descripcion_cliente"></InputError>
                         </div>
                         <div class="col-span-6 sm:col-span-4">
                             <InputLabel for="telefono" value="Telefono" />
-                            <TextInput type="text" name="telefono" id="telefono"></TextInput>
+                            <TextInput type="text" name="telefono" id="telefono" v-model="form.telefono" class="w-full"></TextInput>
+                            <InputError v-bind:message="form.errors.telefono"></InputError>
                         </div>
                         
                     </template>
-                    <template #actions>botonwees</template>
+                    <template #actions>
+                        <button type="submit"> gusardare</button>
+                    </template>
                 </FormSection>
             </div>
         </template>
